b1518348e3a95a5c654ff233f084b6d781ecedcc
Frank Yellin <fy@fyellin.com>
2019-08-14 11:45:01 -0700

    Initial Commit

---
+++ Jewish Calendar/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Jewish Calendar
+//
+//  Created by Frank Yellin on 8/14/19.
+//  Copyright © 2019 Frank Yellin. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
